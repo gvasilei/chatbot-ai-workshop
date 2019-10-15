5dd4ca1de1e986be897b4630e92b695fba77e81f
Giorgos Vasileiou <g.vasileioU@gmail.com>
2019-10-15 10:35:48 +0300

    Initial state

--- chatbot.py
+++ chatbot.py
@@ -1,35 +1,33 @@
 
 import random
 import string
-import logging
-import nltk
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from nltk.stem import WordNetLemmatizer
-from pprint import pformat
-from corpus_loader import CorpusLoader
 
-logging.basicConfig(level=logging.INFO, filename='robo.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+import logging
+from pprint import pformat
+logging.basicConfig(level=logging.INFO)
+
 nltk.data.path.append('./nltk_data')
 
+from corpus_loader import CorpusLoader
 
 class Robo:
     def __init__(self):
+        
+        # Initialization method for Robo. Here you should create any object variables that you might need.
         self.corpusLoader = CorpusLoader()
         self.corpus = self.corpusLoader.load_corpus()
         self.input_sentences = list(self.corpus.keys())
-        logging.debug(pformat(self.corpus))
-    
-
-        self.lemmer = WordNetLemmatizer()
-        self.tfIdfVec = TfidfVectorizer(tokenizer=self.tokenize)
-        self.similarity_threshold = 0.30
-
-        # Keyword Matching
+        
         self.GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey",)
         self.GREETING_RESPONSES = ("hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me")
 
+    
+    
     def lemmatize(self, tokens):
         """
         Lemmatizes a list of words / tokens. Takes as input the list of words and after lemmatizing each one returns a new list with the result.
@@ -40,9 +38,13 @@ class Robo:
         Returns:
             (:obj:`list` of :obj:`str`): A list of lemmatized words
         """
-        return [self.lemmer.lemmatize(token) for token in tokens]
 
-    # Tokenize, convert to lowercase, remove punctuation and then lemmatize
+        # Hint - Check the WordNetLemmatizer class on http://www.nltk.org/api/nltk.stem.html#module-nltk.stem.wordnet
+
+        return []
+    
+    
+    
     def tokenize(self, text):
         """
         Splits (tokenizes) a text into discreet words. Apart from the tokenization it applies some pre and post processsing
@@ -56,8 +58,13 @@ class Robo:
         Returns:
             (:obj:`list` of :obj:`str`): a "bag of words"
         """
-        return self.lemmatize(nltk.word_tokenize(text.lower().translate(str.maketrans('', '', string.punctuation))))
 
+        # Hint - Use https://kite.com/python/docs/nltk.word_tokenize for splitting a text into distinct words
+
+        return []
+
+    
+    
     def isGreeting(self, sentence):
         """
         Checks if the provided sentence is considered a greeting or not.
@@ -68,15 +75,18 @@ class Robo:
         Returns:
             bool: True if the sentence is a greeting and False if not.
         """
-        for word in sentence.split():
-            if word.lower() in self.GREETING_INPUTS: return True
+
+        # Hint - Check if any of the words in the provided sentence is in the GREETING_INPUTS list.
         return False
 
+    
+    
     def greet(self):
         """
         Returns one of the GREETING_RESPONSES at random
         """
-        return random.choice(self.GREETING_RESPONSES)
+        return ""
+
 
     def help(self):
         return """I like telling jokes, gossip and chat in general. I'm pretty knowledgeable about the following topics:
@@ -97,6 +107,7 @@ class Robo:
         """
 
 
+    
     def get_response(self, user_input):
         """
         Takes user input and tries to retrieve an appropriate response.
@@ -107,28 +118,22 @@ class Robo:
         Returns:
             str: The response to give to the user
         """
-        tfidf = self.tfIdfVec.fit_transform(self.input_sentences + [user_input])
 
-        logging.info(self.tfIdfVec.get_feature_names())
-        logging.info(tfidf.shape)
+        # The TF / IDF algorithm, described in the presentation should be used here. Check the following class:
+        # https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.TfidfVectorizer.html and its method fit_transform
+        # 
+        # What we want to do here is to convert all potential input sentences into vertices. 
+        # Then compare the similarity of them with the vertex that represents the actual user input.
+        # The following method that can be used to calculate the similarity between vertices:
+        #
+        # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.cosine_similarity.html
+        
+        return ""
 
-        vals = cosine_similarity(tfidf[-1], tfidf[:-1]).flatten()
-        highest_similarity_idx = vals.argsort()[-1]
-        highest_similarity = vals[highest_similarity_idx]
-
-        if(highest_similarity <= self.similarity_threshold):
-            return "I am sorry! I don't understand you"
-        else:
-            reply_key = self.input_sentences[highest_similarity_idx]
-            logging.debug(self.corpus[reply_key])
-            if len(self.corpus[reply_key]) > 1:
-                return random.choice(self.corpus[reply_key])
-            else:
-                 return self.corpus[reply_key][0]
 
 
 if __name__ == "__main__":
-    print("ROBO: My name is Robo. I will answer your queries about various topics. ask for help if you want to know more. If you want to exit, type Bye!")
+    print("ROBO: My name is Robo. I will answer your queries about various topics. Type 'help' if you want to know more. If you want to exit, type Bye!")
     robo = Robo()
 
     while(True):
@@ -146,6 +151,6 @@ if __name__ == "__main__":
                 print("ROBO: "+robo.greet())
             else:
                 print("ROBO: "+robo.get_response(user_input))
-
+ 
         except(KeyboardInterrupt, SystemExit):
             break
